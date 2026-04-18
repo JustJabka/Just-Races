7e70e9546c14e12e725162160a45bf5b6249e2ba
JustJabka <ivanmartinisin49@gmail.com>
2026-04-19 00:01:38 +0300

    changed item meta to data components

--- src/main/java/justjabka/WeltenRaces/Modifiers/contents/LeatherArmorModifier.java
+++ src/main/java/justjabka/WeltenRaces/Modifiers/contents/LeatherArmorModifier.java
@@ -1,5 +1,7 @@
 package justjabka.WeltenRaces.Modifiers.contents;
 
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.datacomponent.item.ItemAttributeModifiers;
 import justjabka.WeltenRaces.Modifiers.RaceModifier;
 import justjabka.WeltenRaces.WeltenRaces;
 import org.bukkit.NamespacedKey;
@@ -11,6 +13,7 @@ import org.bukkit.persistence.PersistentDataType;
 
 import static justjabka.WeltenRaces.Managers.ItemManager.ITEM_MODIFIED_KEY;
 
+@SuppressWarnings("UnstableApiUsage")
 public class LeatherArmorModifier implements RaceModifier {
     private final String id;
     private final NamespacedKey attributeKey;
@@ -29,21 +32,37 @@ public class LeatherArmorModifier implements RaceModifier {
 
     @Override
     public void apply(ItemStack item) {
-        item.editMeta(meta -> {
-            meta.addAttributeModifier(
-                    Attribute.MOVEMENT_SPEED,
-                    attributeModifier
-            );
+        ItemAttributeModifiers.Builder attributes = ItemAttributeModifiers.itemAttributes();
+                item.getData(DataComponentTypes.ATTRIBUTE_MODIFIERS).modifiers()
+                .stream()
+                .filter(entry -> !entry.modifier().key().equals(attributeKey))
+                .forEach(entry ->
+                    attributes.addModifier(entry.attribute(), entry.modifier(), entry.getGroup(), entry.display())
+                );
 
-            meta.getPersistentDataContainer().set(ITEM_MODIFIED_KEY, PersistentDataType.STRING, id);
-        });
+        attributes.addModifier(Attribute.MOVEMENT_SPEED, attributeModifier);
+
+        item.setData(DataComponentTypes.ATTRIBUTE_MODIFIERS, attributes.build());
+
+        item.editPersistentDataContainer(pdc ->
+                pdc.set(ITEM_MODIFIED_KEY, PersistentDataType.STRING, id)
+        );
     }
 
     @Override
     public void undo(ItemStack item) {
-        item.editMeta(meta -> {
-            meta.removeAttributeModifier(Attribute.MOVEMENT_SPEED, attributeModifier);
-            meta.getPersistentDataContainer().remove(ITEM_MODIFIED_KEY);
-        });
+        ItemAttributeModifiers current = item.getData(DataComponentTypes.ATTRIBUTE_MODIFIERS);
+        if (current != null) {
+            ItemAttributeModifiers.Builder attributes = ItemAttributeModifiers.itemAttributes();
+            current.modifiers().stream()
+                    .filter(entry -> !entry.modifier().key().equals(attributeKey))
+                    .forEach(entry -> attributes.addModifier(entry.attribute(), entry.modifier(), entry.getGroup(), entry.display()));
+
+            item.setData(DataComponentTypes.ATTRIBUTE_MODIFIERS, attributes.build());
+        }
+
+        item.editPersistentDataContainer(pdc ->
+                pdc.remove(ITEM_MODIFIED_KEY)
+        );
     }
 }
