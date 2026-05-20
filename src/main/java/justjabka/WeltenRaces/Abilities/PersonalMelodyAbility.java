9862f15bc6ee0ff2a144ca11926e8b30082f2da2
JustJabka <ivanmartinisin49@gmail.com>
2026-05-20 16:30:02 +0300

    Added epiphyte and phantom buffs

--- src/main/java/justjabka/WeltenRaces/Abilities/PersonalMelodyAbility.java
+++ src/main/java/justjabka/WeltenRaces/Abilities/PersonalMelodyAbility.java
@@ -6,10 +6,14 @@ import justjabka.WeltenRaces.Managers.AbilityManager;
 import justjabka.WeltenRaces.Managers.RaceManager;
 import justjabka.WeltenRaces.WeltenRaces;
 import org.apache.commons.lang3.Range;
+import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -21,22 +25,40 @@ public class PersonalMelodyAbility extends BaseAbility {
     private static final int duration = 30 * 20;
     private static final int requiredNotes = 24;
 
+    // Human
     private static final Set<PotionEffect> humanBuffEffects = Set.of(
             new PotionEffect(PotionEffectType.SATURATION, duration, 0, false, false, true)
     );
 
+    // Armat
     private static final Set<PotionEffect> armatBuffEffects = Set.of(
             new PotionEffect(PotionEffectType.HEALTH_BOOST, duration, 4, false, false, true),
             new PotionEffect(PotionEffectType.REGENERATION, duration, 1, false, false, true)
 
     );
 
+    // Skyzern
     private static final PotionEffect skyzernBuffEffect =
             new PotionEffect(PotionEffectType.STRENGTH, duration, 0, false, false, true);
+
     private static final Range<Integer> skyzernBuffWeak = Range.between(50, 99);
     private static final Range<Integer> skyzernBuffNormal = Range.between(100, 219);
     private static final int skyzernBuffStrong = 220;
 
+    // Epiphyte
+    private static final Set<PotionEffect> epiphyteBuffEffects = Set.of(
+            new PotionEffect(PotionEffectType.STRENGTH, duration, 1, false, false, true)
+    );
+    private static final PotionEffect epiphyteVictimEffect =
+            new PotionEffect(PotionEffectType.WEAKNESS, 10 * 20, 0, false, true, true);
+
+    // Phantom
+    private static final double phantomBuffAdditionalDamage = 6;
+    private static final Set<PotionEffect> phantomBuffEffects = Set.of(
+            new PotionEffect(PotionEffectType.FIRE_RESISTANCE, duration, 0, false, false, true),
+            new PotionEffect(PotionEffectType.INVISIBILITY, duration, 0, false, false, true),
+            new PotionEffect(PotionEffectType.SPEED, duration, 2, false, false, true)
+    );
 
     private NoteAbility getNoteAbility() {
         return AbilityManager.getAbility(NoteAbility.class);
@@ -50,6 +72,32 @@ public class PersonalMelodyAbility extends BaseAbility {
     @Override
     public long getCooldownTicks() {
         return 0;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        if (!AbilityManager.isAbilityActive(player, getKey())) return;
+        unmarkBuffed(player);
+    }
+
+    @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        if (!(event.getDamager() instanceof Player attacker)) return;
+        if (!(event.getEntity() instanceof LivingEntity victim)) return;
+
+        if (!AbilityManager.isAbilityActive(attacker, getKey())) return;
+
+        if (RaceManager.isRace(attacker, RaceProvider.EPIPHYTE)) {
+            victim.addPotionEffect(epiphyteVictimEffect);
+        }
+        else if (RaceManager.isRace(attacker, RaceProvider.PHANTOM)) {
+            event.setDamage(event.getDamage() + phantomBuffAdditionalDamage);
+
+            attacker.removePotionEffect(PotionEffectType.INVISIBILITY);
+            unmarkBuffed(attacker);
+        }
     }
 
     @EventHandler
@@ -77,6 +125,8 @@ public class PersonalMelodyAbility extends BaseAbility {
         if (race.equals(RaceProvider.HUMAN)) giveHumanBuff(target);
         else if (race.equals(RaceProvider.ARMAT)) giveArmatBuff(target);
         else if (race.equals(RaceProvider.SKYZERN)) giveSkyzernBuff(target);
+        else if (race.equals(RaceProvider.EPIPHYTE)) giveEpiphyteBuff(target);
+        else if (race.equals(RaceProvider.PHANTOM)) givePhantomBuff(target);
     }
 
     private void giveHumanBuff(Player player) {
@@ -97,6 +147,25 @@ public class PersonalMelodyAbility extends BaseAbility {
         else return;
 
         player.addPotionEffect(skyzernBuffEffect.withAmplifier(amplifier));
+    }
+
+    private void giveEpiphyteBuff(Player player) {
+        epiphyteBuffEffects.forEach(player::addPotionEffect);
+        markBuffed(player);
+    }
+
+    private void givePhantomBuff(Player player) {
+        phantomBuffEffects.forEach(player::addPotionEffect);
+        markBuffed(player);
+    }
+
+    public void markBuffed(Player player) {
+        AbilityManager.setAbilityState(player, getKey(), true);
+        Bukkit.getScheduler().runTaskLater(WeltenRaces.INSTANCE, () -> unmarkBuffed(player), duration);
+    }
+
+    public void unmarkBuffed(Player player) {
+        AbilityManager.setAbilityState(player, getKey(), false);
     }
 
     @Override
