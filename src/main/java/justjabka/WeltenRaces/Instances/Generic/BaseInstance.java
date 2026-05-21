c1b0abd8a2bbfad21072ad3f4bcf2354ba16a4b2
JustJabka <ivanmartinisin49@gmail.com>
2026-05-21 17:23:19 +0300

    Some base for config right in race

--- src/main/java/justjabka/WeltenRaces/Instances/Generic/BaseInstance.java
+++ src/main/java/justjabka/WeltenRaces/Instances/Generic/BaseInstance.java
@@ -1,16 +1,21 @@
 package justjabka.WeltenRaces.Instances.Generic;
 
+import com.google.gson.JsonObject;
 import justjabka.WeltenRaces.WeltenRaces;
 import org.bukkit.NamespacedKey;
 
 public abstract class BaseInstance {
     private transient String key;
+    private JsonObject config;
 
     public NamespacedKey getKey() {
         return NamespacedKey.fromString(key, WeltenRaces.INSTANCE);
     }
-
     public void setKey(String key) {
         this.key = key;
+    }
+
+    public JsonObject getConfig() {
+        return config;
     }
 }
