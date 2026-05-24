d35a75406d922654f1951068f580d59df64ab52d
JustJabka <ivanmartinisin49@gmail.com>
2026-05-24 23:36:02 +0300

    added proguard

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,81 @@ java {
     toolchain.languageVersion = JavaLanguageVersion.of(21)
 }
 
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.guardsquare:proguard-gradle:7.9.1")
+    }
+}
+
+tasks.register<proguard.gradle.ProGuardTask>("proguard") {
+    description = ""
+    verbose()
+    notCompatibleWithConfigurationCache("ProGuard task is not compatible with Configuration Cache")
+
+    // Take shadowjar
+    injars(tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar").flatMap { it.archiveFile })
+    dependsOn(tasks.named("shadowJar"))
+
+    // Where to save final jar
+    outjars("build/libs/${project.name}-${project.version}-obfuscated.jar")
+
+    val javaHome = System.getProperty("java.home")
+    if (System.getProperty("java.version").startsWith("1.")) {
+        libraryjars("$javaHome/lib/rt.jar")
+    } else {
+        libraryjars(
+            mapOf("jarfilter" to "!**.jar", "filter" to "!module-info.class"),
+            "$javaHome/jmods/java.base.jmod"
+        )
+    }
+
+    // Save mappinggs
+    printmapping("build/proguard-mapping.txt")
+
+    // Don't touch generics
+    keepattributes("Signature,RuntimeVisibleAnnotations,AnnotationDefault,EnclosingMethod,InnerClasses,SourceFile,LineNumberTable")
+
+    // Don't touch main class
+    keep("""
+        public class justjabka.JustRaces.JustRaces {
+            public void onEnable();
+            public void onDisable();
+        }
+    """)
+
+    // Don't obfuscate API
+    keep("""
+        public class justjabka.JustRaces.Registries.** {
+            public protected *;
+        }
+    """)
+    keep("""
+        public class justjabka.JustRaces.Abilities.Generic.** {
+            public protected *;
+        }
+    """)
+    keep("""
+        public class justjabka.JustRaces.Modifiers.ItemModifier {
+            public protected *;
+        }
+    """)
+
+    // Don't obfuscate libs
+    keep("class com.jeff_media.morepersistentdatatypes.** { *; }")
+    keep("class org.spongepowered.configurate.** { *; }")
+
+    dontwarn("org.spongepowered.configurate.**")
+    dontwarn("com.jeff_media.morepersistentdatatypes.**")
+    dontwarn("net.dmulloy2.protocol.**")
+    dontwarn("io.papermc.paper.**")
+    dontwarn("org.bukkit.**")
+    dontwarn("org.apache.lang.**")
+}
+
 tasks {
     shadowJar {
         archiveClassifier.set("")
