d144654cc50356ca0d38957957e870d8afd30d72
JustJabka <ivanmartinisin49@gmail.com>
2026-05-26 11:31:17 +0300

    Updated build workflow

--- justraces-api/build.gradle.kts
+++ justraces-api/build.gradle.kts
@@ -11,7 +11,6 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     compileOnly("org.jetbrains:annotations:24.1.0")
 
-
     compileOnly("net.dmulloy2:ProtocolLib:5.4.0")
     compileOnly("com.jeff-media:MorePersistentDataTypes:2.4.0")
     compileOnly("org.spongepowered:configurate-hocon:4.2.0")
