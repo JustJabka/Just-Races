021456cd6850d982061f2dec23685e278d3997bf
JustJabka <ivanmartinisin49@gmail.com>
2026-06-14 22:37:53 +0300

    Added display for wings. THE POISONOUS POTATO WILL LIVE FUCKING FOREVER IN MY HEART🗣️🔥🔥🔥🔥🔥

--- src/main/java/justjabka/WeltenRaces/Abilities/UnfoldWingsAbility.java
+++ src/main/java/justjabka/WeltenRaces/Abilities/UnfoldWingsAbility.java
@@ -3,11 +3,14 @@ package justjabka.WeltenRaces.Abilities;
 import com.destroystokyo.paper.event.player.PlayerJumpEvent;
 import io.papermc.paper.datacomponent.DataComponentTypes;
 import io.papermc.paper.datacomponent.item.Equippable;
+import io.papermc.paper.datacomponent.item.TooltipDisplay;
 import justjabka.WeltenRaces.Abilities.Generic.BaseValidationAbility;
 import justjabka.WeltenRaces.Configs.Ability.UnfoldWingsAbilityConfig;
 import justjabka.WeltenRaces.Managers.AbilityManager;
 import justjabka.WeltenRaces.Runnables.Ability.UnfoldWingsAbilityRunnable;
 import justjabka.WeltenRaces.WeltenRaces;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.text.Component;
 import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
@@ -24,7 +27,8 @@ import java.util.Map;
 
 @SuppressWarnings("UnstableApiUsage")
 public class UnfoldWingsAbility extends BaseValidationAbility {
-    UnfoldWingsAbilityConfig config;
+    private final UnfoldWingsAbilityConfig config;
+    private static final ItemStack WINGS_ITEM = createWings();
 
     public UnfoldWingsAbility(UnfoldWingsAbilityConfig config) {
         this.config = config;
@@ -77,6 +81,7 @@ public class UnfoldWingsAbility extends BaseValidationAbility {
         Player player = event.getPlayer();
 
         if (!raceHasAbility(player)) return;
+        if (haveItemInChestplateSlot(player)) return;
 
         AttributeInstance jumpStrengthInstance = getJumpStrengthInstance(player);
         if (jumpStrengthInstance == null) return;
@@ -99,20 +104,26 @@ public class UnfoldWingsAbility extends BaseValidationAbility {
 
     /// Gives player ability to fly with wings
     public void giveWings(Player player, AttributeInstance jumpStrengthInstance) {
-        // TODO: Add visual wings
+        if (haveItemInChestplateSlot(player)) return;
+
         AbilityManager.setAbilityState(player, getKey(), true);
 
         jumpStrengthInstance.removeModifier(getKey());
 
-        ItemStack wingsItem = createWings();
-        changeWingsState(player, wingsItem);
+        changeWingsState(player, WINGS_ITEM);
 
         player.setGliding(true);
         new UnfoldWingsAbilityRunnable(this, player.getUniqueId()).runTaskTimer(WeltenRaces.INSTANCE, 10L, 2L);
     }
 
+    private static boolean haveItemInChestplateSlot(Player player) {
+        return !player.getEquipment().getChestplate().isEmpty();
+    }
+
     /// Removes player's ability to fly with wings
     public void removeWings(Player player) {
+        if (!player.getEquipment().getChestplate().isSimilar(WINGS_ITEM)) return;
+
         AbilityManager.setAbilityState(player, getKey(), false);
         changeWingsState(player, ItemStack.empty());
     }
@@ -120,8 +131,8 @@ public class UnfoldWingsAbility extends BaseValidationAbility {
     /// Changes wings item.
     /// Syncs equipment change with server and client
     private static void changeWingsState(Player player, ItemStack item) {
-        player.getEquipment().setItem(EquipmentSlot.SADDLE, item, true);
-        player.sendEquipmentChange(player, EquipmentSlot.SADDLE, item);
+        player.getEquipment().setItem(EquipmentSlot.CHEST, item, true);
+        player.sendEquipmentChange(player, EquipmentSlot.CHEST, item);
     }
 
     /// Creates wings item
@@ -130,13 +141,19 @@ public class UnfoldWingsAbility extends BaseValidationAbility {
                 Material.POISONOUS_POTATO
         );
 
-        // 530000 IQ CRUTCH🧠
+        // 530000 IQ CRUTCH WILL LIVE IN OTHER BRANCHES FOREVER🗣️🔥🔥🔥
         Equippable equippable = Equippable
-                .equippable(EquipmentSlot.SADDLE)
+                .equippable(EquipmentSlot.CHEST)
                 .swappable(false)
+                .assetId(Key.key(WeltenRaces.NAMESPACE, "wings"))
                 .build();
         wings.setData(DataComponentTypes.EQUIPPABLE, equippable);
         wings.setData(DataComponentTypes.GLIDER);
+
+        wings.setData(DataComponentTypes.ITEM_NAME, Component.translatable("item.weltenraces.wings").fallback("Wings"));
+        wings.setData(DataComponentTypes.ITEM_MODEL, new NamespacedKey(WeltenRaces.NAMESPACE, "wings"));
+        wings.setData(DataComponentTypes.TOOLTIP_DISPLAY, TooltipDisplay.tooltipDisplay().addHiddenComponents(DataComponentTypes.ENCHANTMENTS).build());
+        wings.setData(DataComponentTypes.ENCHANTMENT_GLINT_OVERRIDE, false);
 
         wings.addUnsafeEnchantments(Map.of(
                 Enchantment.BINDING_CURSE, 1,
