4e6d0673f490d6cd87e08963289642da5dd0bf8d
JustJabka <ivanmartinisin49@gmail.com>
2026-09-10 17:29:55 +0300

    Updated Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
     id("com.gradleup.shadow") version "9.4.1" apply false
-    id("xyz.jpenilla.run-paper") version "3.0.2" apply false
+    id("xyz.jpenilla.run-paper") version "3.1.0" apply false
 }
