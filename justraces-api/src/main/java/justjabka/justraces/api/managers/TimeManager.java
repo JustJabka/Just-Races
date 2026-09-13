9c4e6eb7bfedf23cc07421276776841c2879b350
JustJabka <ivanmartinisin49@gmail.com>
2026-09-13 18:40:00 +0300

    Added infinite expire stamp

--- justraces-api/src/main/java/justjabka/justraces/api/managers/TimeManager.java
+++ justraces-api/src/main/java/justjabka/justraces/api/managers/TimeManager.java
@@ -6,6 +6,8 @@ public final class TimeManager {
 
     private TimeManager() {}
 
+    public static final long INFINITE_DURATION_STAMP = -1;
+
     /**
      * Gets gametime from the overworld
      * @return Gametime
@@ -15,15 +17,29 @@ public final class TimeManager {
     }
 
     public static long getExpireStamp(long ticks) {
+        if (ticks == INFINITE_DURATION_STAMP) {
+            return INFINITE_DURATION_STAMP;
+        }
+
         return getGameTime() + ticks;
     }
 
     public static long getRemainingExpireStampTicks(long stamp) {
+        if (isExpireStampInfinite(stamp)) {
+            return INFINITE_DURATION_STAMP;
+        }
+
         final long remaining = stamp - getGameTime();
         return Math.max(0, remaining);
     }
 
     public static boolean isExpireStampValid(long stamp) {
+        if (isExpireStampInfinite(stamp)) return true;
+
         return getGameTime() < stamp;
+    }
+
+    public static boolean isExpireStampInfinite(long stamp) {
+        return stamp == INFINITE_DURATION_STAMP;
     }
 }
